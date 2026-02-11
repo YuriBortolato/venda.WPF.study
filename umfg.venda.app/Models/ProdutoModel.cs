7f9f14ae3fb3df6ae7c2ec2c331e4019ecf7b6ee
YuriBortolato <yurigbort@gmail.com>
2026-02-11 20:33:37 -0300

    fix: correçao save item produto

--- umfg.venda.app/Models/ProdutoModel.cs
+++ umfg.venda.app/Models/ProdutoModel.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace umfg.venda.app.Models
 {
-    internal class ProdutoModel
+    public class ProdutoModel
     {
+       
+        private Guid _id = Guid.NewGuid();
+        private ImageSource _image;
+        private string _referencia;
+        private string _descricao;
+        private decimal _valor;
+
+        public Guid Id  { get => _id; set => _id; }
+        public ImageSource Image { get => _image; set => _image = value; }
+        public string Referencia { get => _referencia; set => _referencia = value; }
+        public string Descricao { get => _descricao; set => _descricao = value; }
+        public decimal Valor { get => _valor; set => _valor = value; }
+
     }
 }
